aa16eac0a29dee66a21325808e6be484248b87c1
Rabah-Sebti <sebtirabah19@gmail.com>
2024-01-01 17:25:18 +0100

    third commit

--- countr.js
+++ countr.js
@@ -1,19 +1,19 @@
 //pour ajouter les produits a la base de données
-const express=require('express')
-require('dotenv').config()
-const connectDB=require('./db/connect')
-const Country=require('./models/country')
-const jsonCount=require('./country.json')
-const start=async()=>{
-try {
-    await connectDB(process.env.MONGO_URI)
-    await Country.deleteMany()//supprimer les données a la bd
-    await Country.create(jsonCount)
-    console.log('success data ajouter a BD !!!!!!!');
-    process.exit(0)
-} catch (error) {
+const express = require("express");
+require("dotenv").config();
+const connectDB = require("./db/connect");
+const Country = require("./models/country");
+const jsonCount = require("./country.json");
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    await Country.deleteMany(); //supprimer les données a la bd
+    await Country.create(jsonCount);
+    console.log("success data ajouter a BD !!!!!!!");
+    process.exit(0);
+  } catch (error) {
     console.log(error);
-    process.exit(1)
-}
-}
-start()
+    process.exit(1);
+  }
+};
+start();
